70d189568be2b285f0c47c3db299d6f76569b23b
elunac19 <elunac19@gmail.com>
2024-05-12 14:28:46 -0600

    Changing to csv files

--- dataExtration.py
+++ dataExtration.py
@@ -1,91 +1,99 @@
-import openpyxl 
-from openpyxl import Workbook
+import csv
+import time
 
+#Tienes que quitar todas las celdas convinadas
+#Tienes que quitar las columnas vacias dentro del archivo
+#Tienes que convertir todo a num (opcional)
+#Tienes que remplazar todas las ñ por n
+#Tienes que guardar el archivo como cedulas_comas.csv (separado por comas)
+#tienes que meterte al csv y poner todo en numeros
 
-archivo = openpyxl.load_workbook('cedulas.xlsm')  #Cargar archivo
-hoja = archivo['cedulas'] #Cargar hoja
-nueva_hoja = archivo.create_sheet('ceds_p2')
+def get_unidades(file_csv):
+    unidad_dict = {}
+    unidad = ''
+    initialRow = 0
 
+    with open(file_csv, 'r', encoding='latin-1') as file:
+        csvreader = csv.reader(file)
+        rows = list(csvreader)
+        for i, row in enumerate(rows):
+            for value in row:
+                if value.startswith("Unidad") and unidad == '' and initialRow == 0:
+                    unidad = value
+                    initialRow = i + 3
+                    break
 
-columna_buscada = hoja['B'] #Columna donde se buscaran las unidades
-unidades = ['Unidad: 555/AGUILA',
-            'Unidad: 244/ALCATRAZ',
-            'Unidad: 350/ANGELL',
-            'Unidad: 485/ARCOIRIS II',
-            'Unidad: 327/CAMILA',
-            'Unidad: 221/CORAZON 1',
-            'Unidad: 50/DESAFIO 1',
-            'Unidad: 342/EMYORO',
-            'Unidad: 262/EXITO',
-            'Unidad: 344/FENIX 2',
-            'Unidad: 310/GIRASOLES',
-            'Unidad: 281/GRANDESA',
-            'Unidad: 56/JUAN PABLO',
-            'Unidad: 257/LUPITA 2',
-            'Unidad: 349/LIBELULAS',
-            'Unidad: 146/MAYA 1',
-            'Unidad: 287/MERAKI',
-            'Unidad: 197/NOVA',
-            'Unidad: 328/NUEVO AMANECER',
-            'Unidad: 502/ORQUIDEA',
-            'Unidad: 168/PALOMA 2',
-            'Unidad: 265/RENACIMIENTO',
-            'Unidad: 237/RESPLANDOR II',
-            'Unidad: 171/SATELITE 1', 
-            'Unidad: 124/YARETZY',
-            'Unidad: 556/ZOE',
-            'Unidad: 411/28 DE ENERO',
-            'Unidad: 353/GRATITUD']
+                if value.startswith("Unidad") and unidad != '' and initialRow != 0:
+                    unidad_dict[unidad] = {'initial': initialRow, 'final': i - 3}
+                    unidad = value
+                    initialRow = i + 3
+                    break
 
-nombre_limite = 'Total Unidad' #Identificador de que ahí terminan los datos de la unidad.
+        if unidad != '' and initialRow != 0:
+            unidad_dict[unidad] = {'initial': initialRow, 'final': len(rows) - 6}
 
+    return unidad_dict
 
-global inicio
-global limite
-global rango
-datos_unidad = []
-ini_cel = 0
-columna_max = len(hoja[2])
+def get_sales(file_csv, unidad_dict):
+    mod_rows = []
+    with open(file_csv, 'r', encoding='latin-1') as file:
+        csvreader = csv.reader(file)
+        rows = list(csvreader)
 
-def unidad(valor_c):
-    if valor_c in unidades:
-        return valor_c
-    else:
-        return False
+        for key, values in unidad_dict.items():
+            start = values['initial']
+            end = values['final']
 
-nueva_hoja.append([])
+            for i in range(start, end, 2):
+                current_row = rows[i]
+                previous = rows[i-1]
+                previous[0] = key.replace("Unidad:", "").strip()
+                
+                if current_row[52] == '0.00':
+                    previous[52] = current_row[52] #ARREGLAR ESTO, IDENTIFICA LA ULTIMA SEMANA
 
-for celda in columna_buscada:
-    valor_celda = celda.value
-    if valor_celda and valor_celda.startswith('Uni'):
-        inicio = celda.row
-        nombre = valor_celda
-    elif celda.value == nombre_limite:
-        limite = celda.row
-        
-        if unidad(nombre) !=  False:
-            print(nombre)
+                for index, value in enumerate(current_row):         
+                    if value != '0.00' and value != '':
+                        amount = float(value)
+                        if amount < 0:
+                            if previous[index] == '':
+                                previous[index] += str(amount)
+                            else:
+                                previous[index] = str(round(float(previous[index]) + amount, 2))
+                        else:
+                            previous[index] += str(amount)
 
-            for row in hoja.iter_rows(min_row=inicio+2, max_row=limite-1, max_col=columna_max, values_only=True):
-                updated_row = list(row)
-                updated_row[0] = str(updated_row[0])
-                updated_row[5] = nombre
-                if updated_row[0] != 'None':
-                    datos_unidad.append(updated_row) 
-        
-            #Ordenar datos_unidad en orden decreciente basándose en la primera columna
-            datos_unidad.sort(key=lambda x: -int(x[0]) if x[0].isdigit() else float('inf'), reverse=True)
+                    # print(previous)
+                    # print(f"   Fila: {i+1}")
+                    # print(f"   Indice: {index+1}")
+                    # print(f"   Cantidad a cambiar: {amount}")
+                    # print("------")
+                mod_rows.append(previous)
+            # mod_rows.append('')#quitar para reto
+    return mod_rows
 
-            for row in datos_unidad:
-                if any(row):
-                    nueva_hoja.append(row)
+def new_csv(file_csv, mod_rows):
+    new_csv = file_csv.replace('.csv', '_mod.csv')
 
-            calc_cel = ini_cel + 1
-            nombre_cel = nueva_hoja['B' + str(calc_cel)]
-            nombre_cel.value = nombre
-            ini_cel = len(nueva_hoja['B'])
-            datos_unidad.clear()
-            nueva_hoja.append([])
-            nueva_hoja.append([])
+    with open(new_csv, 'w', newline='', encoding='latin-1') as new_file:
+        csvwriter = csv.writer(new_file)
+        csvwriter.writerows(mod_rows)
 
-archivo.save('orden2.xlsx')
+    print(f"Se ha guardado el archivo modificado en: {new_csv}")
+                
+# Registra el tiempo de inicio
+start_time = time.time()
+
+file_csv = '../INPUT/rpt_VentasComisionables_SinCorte.csv'
+unidad_dict = get_unidades(file_csv)
+
+print("Unidades encontradas:")
+for key, values in unidad_dict.items():
+    print(f"{key}: [{values['initial']}, {values['final']}]")
+
+mod_rows = get_sales(file_csv, unidad_dict)
+new_csv(file_csv, mod_rows)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Tiempo de ejecución: {elapsed_time} segundos")
